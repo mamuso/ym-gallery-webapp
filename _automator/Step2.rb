5257b9909bdf2a6a708e129f4646e7c7055bee38
Manuel Muñoz Solera <mamuso@mamuso.net>
2014-11-17 17:14:27 +0000

    Adding Step 2

---
+++ _automator/Step2.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+Encoding.default_external = "utf-8"
+
+require 'rubygems'
+require 'json'
+require 'cgi'
+require 'fileutils'
+require 'pathname'
+
+# ===============================================
+# 
+#   Copy Folders
+# 
+# ===============================================
+
+dropbox_json = File.read(ENV['HOME']+'/.dropbox/info.json')
+dropbox = JSON.parse(dropbox_json)['business']['path']
+dsbuilder = dropbox+'/DesignSite'
+
+# This is really poorly done, but... 
+if Dir.exist?(dsbuilder)
+  FileUtils.cp_r(dsbuilder+"/projects", "/tmp/ymgallery/assets")
+end
