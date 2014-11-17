f8e472685874ca4ff718d80bb6abda815baebac0
Manuel Muñoz Solera <mamuso@mamuso.net>
2014-11-17 17:49:55 +0000

    Fixing model routing, ignoring projects

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   buildURL: function(type, id) {
-    var url = '/assets/' + Ember.String.pluralize(type);
+    var url = Ember.String.pluralize(type);
     if (id) {
         url += '/' + id + '/yamproject';
     }
