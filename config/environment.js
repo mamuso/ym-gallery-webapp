219e527ed0105bde5701bd751527f434678a4494
Manuel Muñoz Solera <mamuso@mamuso.net>
2014-11-20 18:52:19 +0000

    getting rid of dependencies

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = '/ym-gallery-webapp/'
+    ENV.baseURL = '/pages/yammer/design/'
   }
 
   return ENV;
