1684262627fdba80cc5d271791511a5995399a40
Manuel Muñoz Solera <mamuso@mamuso.net>
2014-11-21 18:52:36 +0000

    workarounds

--- app/controllers/project.js
+++ app/controllers/project.js
@@ -3,16 +3,15 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   img: null,
   hide: true,
-  freeze: false,
   actions: {
     open: function(img) {
       this.set('hide', false); 
-      this.set('freeze', true); 
       this.set('img', img); 
+      Ember.$("body").addClass("freeze");
     },
     close: function() {
       this.set('hide', true); 
-      this.set('freeze', false); 
+      Ember.$("body").removeClass("freeze");
     }
 
   }
