8dfd516816279e5d0d47f1dbb63c9be881a9f951
Manuel Muñoz Solera <mamuso@mamuso.net>
2014-11-22 00:39:01 +0000

    Fixing overlay

--- app/components/project-thumbnail.js
+++ app/components/project-thumbnail.js
@@ -5,34 +5,35 @@ export default Ember.Component.extend({
     var value = this.get("value"),
         folder = this.get("folder"),
         id = this.get("project_id"),
-        route = "",
         spvalue = value.split("."),
         ext = spvalue.pop(),
-        file = spvalue.join(".")+"_yamthumbnail."+ext;
-
-    if(id === folder) {
-      route = "assets/projects/"+id+"/"+file;
-    } else {
-      route = "assets/projects/"+id+"/"+folder+"/"+file;
-    }
+        file = spvalue.join(".")+"_yamthumbnail."+ext,
+        route = this.getURL(file, folder, id);
     return route;
   }.property( 'value' , 'folder' ),
+
   bigUrl: function () {
     var file = this.get("value"),
         folder = this.get("folder"),
         id = this.get("project_id"),
-        route = "";
+        route = this.getURL(file, folder, id);
 
+    return route;
+  }.property( 'value' , 'folder' ),
+
+  getURL: function(file, folder, id) {
+    var route = "";
     if(id === folder) {
       route = "assets/projects/"+id+"/"+file;
     } else {
       route = "assets/projects/"+id+"/"+folder+"/"+file;
     }
     return route;
-  }.property( 'value' , 'folder' ),
+  },
+
   actions: {
-    open: function() {
-      this.sendAction('open', this.get('bigUrl'));
+    open: function(bigUrl) {
+      this.sendAction('open', bigUrl);
     }
   }
 });
